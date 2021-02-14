6ea83dd29a4e77df6b2af7f8fd4a329fee8a2e9a
matovu-farid <64433642+matovu-farid@users.noreply.github.com>
2021-02-15 01:27:40 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/faridmatovu/food_tracing_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.faridmatovu.food_tracing_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.teal,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Food Tracer'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Center(child: Text(widget.title)),
+      ),
+      body: Center(
+
+
+      ),
+     );
+  }
+}
